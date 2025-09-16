82fccd6e109096c96fd64c621826e403c0e32196
Ignacio Pickering <ign.pickering@gmail.com>
2025-09-16 00:24:17 -0400

    Add refine_inplace function (#30)

--- bblean/__init__.py
+++ bblean/__init__.py
@@ -3,5 +3,6 @@ bblean
 """
 
 from bblean.bitbirch import BitBirch, set_merge  # type: ignore
+from bblean.packing import pack_fingerprints, unpack_fingerprints
 
-__all__ = ["BitBirch", "set_merge"]
+__all__ = ["BitBirch", "set_merge", "pack_fingerprints", "unpack_fingerprints"]
