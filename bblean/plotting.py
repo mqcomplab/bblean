f5bb2f5ca12e261aa7725f45bb7ad3e5c3b07bfe
Ignacio Pickering <ign.pickering@gmail.com>
2025-09-29 03:47:34 -0400

    feature/ipickering/tsne plotting (#79)

--- bblean/plotting.py
+++ bblean/plotting.py
@@ -1,6 +1,5 @@
 r"""Plotting and visualization convenience functions"""
 
-from numpy.typing import NDArray
 import typing as tp
 
 import numpy as np
@@ -8,12 +7,15 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from rdkit import Chem
 from rdkit.Chem import Draw
-from sklearn.manifold import TSNE
-from sklearn.preprocessing import StandardScaler
+import colorcet
+from sklearn.preprocessing import StandardScaler, normalize as normalize_features
+from sklearn.decomposition import PCA
+from openTSNE.sklearn import TSNE
+from openTSNE.affinity import Multiscale
 
-from bblean.utils import batched
+from bblean.utils import batched, _num_avail_cpus
 from bblean.analysis import ClusterAnalysis
-from bblean.fingerprints import unpack_fingerprints
+from bblean._config import TSNE_SEED
 
 __all__ = ["summary_plot", "tsne_plot", "dump_mol_images"]
 
@@ -27,7 +29,9 @@ def summary_plot(
     title: str | None = None,
     annotate: bool = True,
 ) -> tuple[plt.Figure, tuple[plt.Axes, ...]]:
-    r"""Obtain a summary plot from a cluster analysis"""
+    r"""Create a summary plot from a cluster analysis.
+
+    If the analysis contains scaffolds, a scaffold analysis is added to the plot"""
     orange = "tab:orange"
     blue = "tab:blue"
     df = c.df
@@ -127,42 +131,99 @@ def summary_plot(
 
 
 def tsne_plot(
-    c: ClusterAnalysis, /, title: str | None = None
+    c: ClusterAnalysis,
+    /,
+    title: str | None = None,
+    seed: int | None = TSNE_SEED,
+    perplexity: int = 30,
+    workers: int | None = None,
+    scaling: str = "normalize",
+    exaggeration: float | None = None,
+    do_pca_init: bool = True,
+    multiscale: bool = False,
+    pca_reduce: int | None = None,
+    dof: float = 1.0,
 ) -> tuple[plt.Figure, tuple[plt.Axes, ...]]:
-    r"""Obtain a t-SNE plot from a cluster analysis"""
+    r"""Create a t-SNE plot from a cluster analysis"""
+    if workers is None:
+        workers = _num_avail_cpus()
     df = c.df
-    fps_list: list[NDArray[np.uint8]] = []
-    label_list: list[int] = []
-    cluster_fps: list[NDArray[np.uint8]]
-    for fps, label in zip(c.fps, df["label"]):
-        if c.fps_are_packed:
-            # fps is 2D so this is guaranteed to be a list of u8 arrays
-            # Too complicated for mypy to parse, but correct
-            cluster_fps = [
-                fp for fp in unpack_fingerprints(fps, n_features=c.n_features)  # type: ignore # noqa:E501
-            ]
-        else:
-            cluster_fps = list(fps)
-        fps_list.extend(cluster_fps)
-        label_list.extend([label] * len(cluster_fps))
+    color_labels: list[int] = []
+    for num, label in zip(df["mol_num"], df["label"]):
+        color_labels.extend([label - 1] * num)  # color labels start with 0
     num_clusters = c.num_clusters
-    scaler = StandardScaler()
-    fps_scaled = scaler.fit_transform(fps_list)
 
-    tsne = TSNE(n_components=2, perplexity=30, random_state=42)
-    fps_tsne = tsne.fit_transform(fps_scaled)
+    # I don't think these should be transformed, like this, only normalized
+    if scaling == "normalize":
+        fps_scaled = normalize_features(c.unpacked_fps)
+    elif scaling == "std":
+        scaler = StandardScaler()
+        fps_scaled = scaler.fit_transform(c.unpacked_fps)
+    elif scaling == "none":
+        fps_scaled = c.unpacked_fps
+    else:
+        raise ValueError(f"Unknown scaling {scaling}")
+    if pca_reduce is not None:
+        fps_scaled = PCA(n_components=pca_reduce).fit_transform(fps_scaled)
 
-    fig, ax = plt.subplots(figsize=(8, 6))
-    scatter = ax.scatter(
-        fps_tsne[:, 0], fps_tsne[:, 1], c=label_list, cmap="tab20", alpha=0.9
+    # Learning rate is set to N / exaggeration (good default)
+    # Early exaggeration defaults to max(12, exaggeration) (good default)
+    # exaggeration_iter = 250, normal_iter = 500 (good defaults)
+    # "pca" is the method used by Dimitry Kovak et. al. (good default), with some jitter
+    # added for extra numerical stability
+    # Multiscale may help with medium-sized datasets together with downsampling, but
+    # it doesn't do much in my tests.
+    # NOTE: Dimensionality reduction with PCA to ~50 features seems to mostly preserve
+    # cluster structure
+    tsne = TSNE(
+        n_components=2,
+        perplexity=perplexity,
+        random_state=seed,
+        n_jobs=workers,
+        dof=dof,
+        exaggeration=exaggeration,  # second-phase exaggeration
+        negative_gradient_method="fft",  # faster for large datasets
+        initialization="pca" if do_pca_init else "random",
     )
-    cbar = plt.colorbar(scatter, label="Cluster ID")
+    if multiscale:
+        fps_tsne = (
+            super(TSNE, tsne)
+            .fit(
+                fps_scaled,
+                affinities=Multiscale(
+                    n_jobs=workers,
+                    random_state=seed,
+                    data=fps_scaled,
+                    perplexities=[perplexity, len(fps_scaled) / 100],
+                ),
+                initialization="pca" if do_pca_init else "random",
+            )
+            .view(np.ndarray)
+        )
+    else:
+        fps_tsne = tsne.fit_transform(fps_scaled)
+
+    fig, ax = plt.subplots(dpi=250, figsize=(4, 3.5))
+    scatter = ax.scatter(
+        fps_tsne[:, 0],
+        fps_tsne[:, 1],
+        c=color_labels,
+        cmap=mpl.colors.ListedColormap(colorcet.glasbey_bw_minc_20[:num_clusters]),
+        edgecolors="none",
+        alpha=0.5,
+        s=2,
+    )
+    # t-SNE plots *must be square*
+    ax.set_aspect("equal", adjustable="box")
+    cbar = plt.colorbar(scatter, label="Cluster label")
     cbar.set_ticks(list(range(num_clusters)))
     cbar.set_ticklabels(list(map(str, range(1, num_clusters + 1))))
-    ax.set_xlabel("t-SNE Component 1")
-    ax.set_ylabel("t-SNE Component 2")
+    ax.set_xlabel("t-SNE component 1")
+    ax.set_ylabel("t-SNE component 2")
+    msg = f"t-SNE of top {num_clusters} largest clusters"
     if title is not None:
-        fig.suptitle(f"t-SNE of Top {num_clusters} Largest Clusters for {title}")
+        msg = f"{msg} for {title}"
+    fig.suptitle(msg)
     return fig, (ax,)
 
 
