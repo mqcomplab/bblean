f1679f37846b996d92454608648ffe2d060457b7
klopezperez <klopezperez@ufl.edu>
2025-10-22 17:38:58 -0400

    white spaces

--- bblean/_py_similarity.py
+++ bblean/_py_similarity.py
@@ -82,9 +82,9 @@ def jt_compl_isim(
     return np.array(comp_sims, dtype=np.float64)
 
 
-def jt_stratified_sampling(fps: NDArray[np.uint8], 
-    n_samples: int, 
-    input_is_packed: bool = True, 
+def jt_stratified_sampling(fps: NDArray[np.uint8],
+    n_samples: int,
+    input_is_packed: bool = True,
     n_features: int | None = None
 ) -> NDArray[np.uint8]:
     # Calculate the complementary similarities
