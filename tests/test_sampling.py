4a6372f337c8004d088d5c192d08b4f12747d908
ipickering <ign.pickering@gmail.com>
2025-10-23 17:11:18 -0400

    Add docs

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -8,12 +8,12 @@ from inline_snapshot import snapshot
 
 def test_stratified() -> None:
     fps = make_fake_fingerprints(10, seed=1345)
-    out = jt_stratified_sampling(fps, 3, seed=1234)
-    assert out.tolist() == snapshot([5, 6, 7])
+    out = jt_stratified_sampling(fps, 3)
+    assert out.tolist() == snapshot([0, 4, 2])
 
-    assert jt_stratified_sampling(fps, 0, seed=1234).size == 0
+    assert jt_stratified_sampling(fps, 0).size == 0
     assert (
-        jt_stratified_sampling(fps, 10, seed=1234) == np.arange(10, dtype=np.int64)
+        jt_stratified_sampling(fps, 10) == np.arange(10, dtype=np.int64)
     ).all()
     with pytest.raises(ValueError):
-        _ = jt_stratified_sampling(fps, 11, seed=1234)
+        _ = jt_stratified_sampling(fps, 11)
